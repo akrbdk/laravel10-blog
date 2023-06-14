444b25093cff1b3305d49f195320c179f20f1db4
alexander.karabudka <sanyakarabudka@gmail.com>
2023-06-14 22:23:55 +0300

    estimated read time

--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -20,7 +20,8 @@
                 </h1>
 
                 <p href="#" class="text-sm pb-8">
-                    By <a href="#" class="font-semibold hover:text-gray-800">{{ $post->user->name }}</a>, Published on {{ $post->getFormattedDate() }}
+                    By <a href="#" class="font-semibold hover:text-gray-800">{{ $post->user->name }}</a>,
+                    Published on {{ $post->getFormattedDate() }} | {{ $post->human_read_time }}
                 </p>
 
                 <div>
